1f7cc05d476c6a8f34a2949d8ba8bc394323a508
justinwozniak <wozzytheprogrammer@gmail.com>
2021-06-11 08:45:23 -0400

    Updated main image

---
+++ src/components/Videos/Interviews.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import VideoComponent from './VideoComponent';
+import './videos.css';
+const Interviews = (props) => {
+	return (
+		<div>
+			<h1 className="video-header">Interviews</h1>
+			<div className="trailers-wrapper">
+				<VideoComponent
+					title="A Nightmare on Elm Street (1984)"
+					url="https://www.youtube.com/watch?v=ZuYoEtEI_go"
+				/>
+				<VideoComponent title="Private Resort (1985)" url="https://www.youtube.com/watch?v=o_TSNE9MRBQ" />
+				<VideoComponent title="Platoon (1986)" url="https://www.youtube.com/watch?v=KztP7SKe0uk" />
+				<VideoComponent title="Cry-Baby (1990)" url="https://www.youtube.com/watch?v=yB1bLlguZVY" />
+				<VideoComponent title="Edward Scissorhands (1990)" url="https://www.youtube.com/watch?v=7iTFYsmFV_Q" />
+				<VideoComponent title="Arizona Dream (1993)" url="https://www.youtube.com/watch?v=xR4Aq-p8thE" />
+				<VideoComponent title="Benny and Joon (1993)" url="https://www.youtube.com/watch?v=oPjBb1qeiK4" />
+				<VideoComponent
+					title="What's Eating Gilbert Grape (1993)"
+					url="https://www.youtube.com/watch?v=X6sLIP3908w"
+				/>
+				<VideoComponent title="Ed Wood (1994)" url="https://www.youtube.com/watch?v=sqFPKpzGimI" />
+				<VideoComponent title="Don Juan DeMarco (1994)" url="https://www.youtube.com/watch?v=Ymyo4e_sjwY" />
+
+				<VideoComponent title="Dead Man (1995)" url="https://www.youtube.com/watch?v=nn2nQMfLqwg" />
+				<VideoComponent title="Nick of Time (1995)" url="https://www.youtube.com/watch?v=0K8iqPgUmBQ" />
+				<VideoComponent title="Donnie Brasco (1997)" url="https://www.youtube.com/watch?v=ilJ_ROLClZQ" />
+				<VideoComponent title="The Brave (1997)" url="https://www.youtube.com/watch?v=GkfnxBOiPKQ" />
+				<VideoComponent
+					title="Fear and Loathing in Las Vegas (1998)"
+					url="https://www.youtube.com/watch?v=8m662obIvhY&t=2s"
+				/>
+				<VideoComponent title="The Ninth Gate (1999)" url="https://www.youtube.com/watch?v=IKc1n-nIIx0" />
+				<VideoComponent title="The Astronaut's Wife (1999)" url="https://www.youtube.com/watch?v=MyyGOmsIdFI" />
+				<VideoComponent title="Sleepy Hollow (1999)" url="https://www.youtube.com/watch?v=R6O4Himch7g" />
+				<VideoComponent title="The Man Who Cried (2000)" url="https://www.youtube.com/watch?v=jxcHiy4q_rI" />
+				<VideoComponent title="Before Night Falls (2000)" url="https://www.youtube.com/watch?v=wEDGERJQiOE" />
+
+				<VideoComponent title="Chocolat (2000)" url="https://www.youtube.com/watch?v=nn2nQMfLqwg" />
+				<VideoComponent title="Nick of Time (1995)" url="https://www.youtube.com/watch?v=692hOJq1KJE" />
+				<VideoComponent title="Blow (2001)" url="https://www.youtube.com/watch?v=6n71j-ZV_JI" />
+				<VideoComponent title="From Hell (2001)" url="https://www.youtube.com/watch?v=i2WufNVIdOo" />
+				<VideoComponent
+					title="Pirates of the Caribbean: The Curse of the Black Pearl (2003)"
+					url="https://www.youtube.com/watch?v=gPBaZpDONgg"
+				/>
+				<VideoComponent
+					title="Once Upon a Time in Mexico (2003)"
+					url="https://www.youtube.com/watch?v=7JrCqCz6CBA"
+				/>
+				<VideoComponent title="Secret Window (2004)" url="https://www.youtube.com/watch?v=1lF7CSXCMws" />
+				<VideoComponent title="Finding Neverland (2004)" url="https://www.youtube.com/watch?v=cVLdNA2Qetk" />
+				<VideoComponent title="The Libertine (2004)" url="https://www.youtube.com/watch?v=1yBRjrB2Ih4" />
+				<VideoComponent
+					title="Charlie and the Chocolate Factory (2005)"
+					url="https://www.youtube.com/watch?v=p6JAQ2aoXm0"
+				/>
+
+				<VideoComponent title="Corpse Bride (2005)" url="https://www.youtube.com/watch?v=G9boDkpEyvc" />
+				<VideoComponent
+					title="Pirates of the Caribbean: Dead Man's Chest (2006)"
+					url="https://www.youtube.com/watch?v=iPKbf7H8qC8"
+				/>
+				<VideoComponent
+					title="Pirates of the Caribbean: At World's End (2007)"
+					url="https://www.youtube.com/watch?v=6n71j-ZV_JI"
+				/>
+				<VideoComponent title="The Tourist (2010)" url="https://www.youtube.com/watch?v=5XtbLezJtMg" />
+				<VideoComponent title="Rango (2011)" url="https://www.youtube.com/watch?v=k-OOfW6wWyQ" />
+				<VideoComponent
+					title="Pirates of the Caribbean: On Stranger Tides (2011)"
+					url="https://www.youtube.com/watch?v=0BXCVe8Yww4"
+				/>
+				<VideoComponent title="The Rum Diary (2011)" url="https://www.youtube.com/watch?v=R8KblG1S0_A" />
+				<VideoComponent title="Dark Shadows (2012)" url="https://www.youtube.com/watch?v=N6tVdffCr_M" />
+				<VideoComponent title="The Lone Ranger (2013)" url="https://www.youtube.com/watch?v=8xv6NmOWw30" />
+				<VideoComponent title="Transcendence (2014)" url="https://www.youtube.com/watch?v=QheoYw1BKJ4" />
+
+				<VideoComponent title="Tusk (2014)" url="https://www.youtube.com/watch?v=BCQJnOn0ru0" />
+				<VideoComponent title="Black Mass (2015)" url="https://www.youtube.com/watch?v=R_F-lVhSfx8" />
+				<VideoComponent title="Yoga Hosers (2016)" url="https://www.youtube.com/watch?v=5EAy6-uCb1o" />
+				<VideoComponent
+					title="Alice Through the Looking Glass (2016)"
+					url="https://www.youtube.com/watch?v=sfwTByCyotA"
+				/>
+				<VideoComponent
+					title="Fantastic Beasts and Where to Find Them (2016)"
+					url="https://www.youtube.com/watch?v=ViuDsy7yb8M"
+				/>
+				<VideoComponent
+					title="Pirates of the Caribbean: Dead Men Tell No Tales (2017)"
+					url="https://www.youtube.com/watch?v=Hgeu5rhoxxY"
+				/>
+				<VideoComponent
+					title="Murder on the Orient Express (2017)"
+					url="https://www.youtube.com/watch?v=Mq4m3yAoW8E"
+				/>
+				<VideoComponent title="Sherlock Gnomes (2018)" url="https://www.youtube.com/watch?v=zC9b1orltc8" />
+				<VideoComponent title="The Professor (2018)" url="https://www.youtube.com/watch?v=IThXCpk3Oc4" />
+				<VideoComponent
+					title="Fantastic Beasts: The Crimes of Grindelwald (2018)"
+					url="https://www.youtube.com/watch?v=vvFybpmyB9E"
+				/>
+
+				<VideoComponent title="City of Lies (2018)" url="https://www.youtube.com/watch?v=rTxobgjD3hE" />
+				<VideoComponent
+					title="Waiting for the Barbarians (2019)"
+					url="https://www.youtube.com/watch?v=PHE5iHeYBqw"
+				/>
+				<VideoComponent title="Minamata (2020)" url="https://www.youtube.com/watch?v=WP3pKTssw_E" />
+			</div>
+		</div>
+	);
+};
+
+export default Interviews;
--- src/components/Videos/index.js
+++ src/components/Videos/index.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import MovieTrailers from './MovieTrailers.js';
 import MainHeader from '../../components/MainHeader';
+import Interviews from './Interviews'
 const Videos = (props) => {
 	const [ answer, setAnswer ] = useState('');
 
 	let handleTrailersSubmit = (event) => {
 		setAnswer('movieTrailers');
+	};
+	let handleInterviewsSubmit = (event) => {
+		setAnswer('Interviews');
 	};
 	return (
 		<div>
@@ -14,8 +18,12 @@ const Videos = (props) => {
 			<button onClick={handleTrailersSubmit} name="movieTrailers" className="btn41-43 btn-41" >
 				Movie Trailers
 			</button>
+			<button onClick={handleInterviewsSubmit} name="Interviews" className="bn5" >
+				Interviews
+			</button>
 			</div>
 			<div className="videos-wrapper">{answer === 'movieTrailers' && <MovieTrailers />}</div>
+			<div className="videos-wrapper">{answer === 'Interviews' && <Interviews />}</div>
 		</div>
 	);
 };
